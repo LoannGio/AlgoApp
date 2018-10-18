96f9bd92c2bc4c259a44d108b545db9454f06662
fabienjacques <fabien.jacques@etu.u-bordeaux.fr>
2018-10-18 09:13:27 +0200

    Crétaion de la classe SubsetCreator qui contiendra une fonction statique qui génère tous les sous-ensemble de taille n d'un ensemble

---
+++ robotdef/src/main/SubsetCreator.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.List;
+import java.util.Set;
+
+public class SubsetCreator {
+	
+	public static <V> List<Set<V>> allSubsetsOfSizeN(Set<V> S, int n){
+		
+		
+		
+		return null;
+	}
+}
