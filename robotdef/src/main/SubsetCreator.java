fc931a469fc3bdfb8c8ac5dd89ce885e4a972f35
jnarboni <jonathan.narboni@etu.u-bordeaux.fr>
2018-10-27 11:27:11 +0200

    Subsets function: recursively computes all subsets of size k of a set

--- robotdef/src/main/SubsetCreator.java
+++ robotdef/src/main/SubsetCreator.java
@@ -2,8 +2,11 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
+import java.util.Vector;
 
 public class SubsetCreator {
 
@@ -50,4 +53,64 @@ public class SubsetCreator {
 		recurs_subsetsOfSizeR(vertexes, vertexes.size(), n, 0, subset, 0, subsets);
 		return subsets;
 	}
+	
+	//getting the set of all subset of size k composed with elements of original set
+	public static <V> List<List<V>> subsets(List<V> set,int k,V start )
+	{
+		//System.out.println("set of size:" + k + ":: from : " + start);
+		//set of size :k, composed of element of :set, starting at element :start.
+		List<List<V>> subsets = new Vector<>();
+		//k=0 => returning singleton "emptyset"
+		if(k == 0)
+		{
+			subsets.add(new Vector<>());
+			return subsets;
+		}
+		//iterating on the list until finding element start
+		ListIterator<V> it = set.listIterator();
+		V v = null;
+		while(it.hasNext())
+		{
+			v = it.next();
+			if(v == start)
+				break;
+			
+		}
+		//backing one step to retrieve the iterator of element start (not so nice but
+		//didn't find out how to do it a better way: this is the reason for 
+		//using List Interface)
+		it.previous();
+		while(it.hasNext())
+		{
+			v = it.next();
+			//recursion: getting the k-1 sized subsets starting at start+1
+			List<List<V>> smallerSubsets = subsets(set,k-1,v);
+			//adding element start to every subsets of size k-1 => all subsets are of size k
+			for(List<V> s : smallerSubsets)
+			{
+				//check for unicity of element in the set(not so nice either, but 
+				//the Vector class does not implement Set Interface)
+				if(!s.contains(v))
+				{
+					s.add(v);
+					subsets.add(s);
+				}
+			}
+		}
+		return subsets;
+	}
+	
+	/*public static void main(String[] args)
+	{
+		List<Integer> testSet = new Vector<>();
+		for(int i =0;i<11; i++)
+		{
+			testSet.add(i);
+		}
+		
+		System.out.println(testSet);
+		List<List<Integer>> subsetsSizeK = subset(testSet, 3, 0);
+		System.out.println("size:" + subsetsSizeK.size());
+		System.out.println(subsetsSizeK);
+	}*/
 }
