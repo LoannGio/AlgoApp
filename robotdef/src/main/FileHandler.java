be4e10d7ca2f0004a3eec6413ccc060ebcd85f7e
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-11-08 09:54:30 +0100

    filehandler propre

--- robotdef/src/main/FileHandler.java
+++ robotdef/src/main/FileHandler.java
@@ -1,13 +1,14 @@
 package main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileHandler {
 
-	public static String openFile() {
+	public static String openFile() throws FileNotFoundException {
 		String filepath = "";
 		JFileChooser chooser = new JFileChooser();
 		File workingDirectory = new File(System.getProperty("user.dir") + "/problems");
@@ -20,9 +21,9 @@ public class FileHandler {
 		if (retValue == JFileChooser.APPROVE_OPTION) {
 			filepath = chooser.getSelectedFile().getPath();
 		} else {
-			filepath = "File not found"; // ou throw new
-											// FileNotFoundException(), mais de
-											// toute facon
+			filepath = null; // ou throw new
+								// FileNotFoundException(), mais de
+								// toute facon
 			// si il n y a
 			// pas de fichier on execute pas...
 		}
--- robotdef/src/main/Main.java
+++ robotdef/src/main/Main.java
@@ -1,19 +1,27 @@
 package main;
 
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 import org.json.JSONException;
 
 public class Main {
-	public static void main(String[] args) throws JSONException {
+	public static void main(String[] args) throws JSONException, FileNotFoundException {
 
 		String filepath = FileHandler.openFile();
+		if (filepath == null) {
+			System.out.println("File not found");
+			return;
+		}
 		RGraph G = new RGraph(filepath, false);
 
 		// Test de smallestDominatingSet()
 		System.out.println("---Test de smallestDominatingSet()");
-		Set<RVertex> solution = (Set<RVertex>) Domination.smallestDominatingSetBruteForce(G, G.getShotLineVertices(),
-				G.getPositionVertices(), false);
+		long time = System.currentTimeMillis();
+		Set<RVertex> solution = (Set<RVertex>) Domination.dominatingSetGreedy(G, G.getShotLineVertices(),
+				G.getPositionVertices());
+		time = System.currentTimeMillis() - time;
+		System.out.println("Duree de la generation de la solution (ms) : " + time);
 		JSonSolution.saveJSonSolution(solution);
 		System.out.println(solution);
 		System.out.println(G.getPositionVertices());
