e7dc18c142397ca2262d0823c8badd7697161f8a
Jonathan Narboni <jnarboni@emi.u-bordeaux1.fr>
2018-11-19 17:43:43 +0100

    commit useless

--- robotdef/src/main/Main.java
+++ robotdef/src/main/Main.java
@@ -1,33 +1,30 @@
 package main;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Set;
 
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import org.json.JSONException;
 
-public class FileHandler {
+public class Main {
+	public static void main(String[] args) throws JSONException, FileNotFoundException {
 
-	public static String openFile() throws FileNotFoundException {
-		String filepath = "";
-		JFileChooser chooser = new JFileChooser();
-		File workingDirectory = new File(System.getProperty("user.dir") + "/problems");
-		chooser.setCurrentDirectory(workingDirectory);
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON files (*.json)", "json");
-
-		chooser.setFileFilter(filter);
-
-		int retValue = chooser.showOpenDialog(null);
-		if (retValue == JFileChooser.APPROVE_OPTION) {
-			filepath = chooser.getSelectedFile().getPath();
-		} else {
-			filepath = null; // ou throw new
-								// FileNotFoundException(), mais de
-								// toute facon
-			// si il n y a
-			// pas de fichier on execute pas...
+		String filepath = FileHandler.openFile();
+		if (filepath == null) {
+			System.out.println("File not found");
+			return;
 		}
-		return filepath;
-	}
+		RGraph G = new RGraph(filepath, false);
 
+		// Test de smallestDominatingSet()
+		System.out.println("---Test de smallestDominatingSet()");
+		long time = System.currentTimeMillis();
+		Set<RVertex> solution = (Set<RVertex>) Domination.dominatingSetGreedy(G, G.getShotLineVertices(),
+				G.getPositionVertices());
+		time = System.currentTimeMillis() - time;
+		System.out.println("Duree de la generation de la solution (ms) : " + time);
+		JSonSolution.saveJSonSolution(solution);
+		System.out.println(solution);
+		System.out.println(G.getPositionVertices());
+		new RGraphVisualizer(G);
+	}
 }
